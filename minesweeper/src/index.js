8954224c38fd729b9357111bfbcaf724fd816559
Dallin Stirling <dallinks@byu.edu>
2022-05-09 17:27:03 -0600

    Foundation of the app

--- minesweeper/src/index.js
+++ minesweeper/src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Square extends React.Component {
+  constructor(props){
+    super(props);
+    const isMine = Math.random() * 10 % 2
+    if(isMine === 1){
+      isMine = true
+    }
+    else {
+      isMine = false
+    }
+    this.state = {
+      isMine:isMine,
+    }
+  }
+  render(){
+    return (<button className='square' onClick={() => this.setState()}>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    </button>)
+  }  
+}
+
+class Game extends React.Component {
+  renderSquare(i){
+    return <Square />
+  }
+  render(){
+    return (
+      <div className='Board'>
+        <div className='boardRow'>
+        {this.renderSquare(1)}
+        {this.renderSquare(2)}
+        {this.renderSquare(3)}
+        </div>
+        <div>
+        {this.renderSquare(4)}
+        {this.renderSquare(5)}
+        {this.renderSquare(6)}
+        </div>
+        <div>
+        {this.renderSquare(7)}
+        {this.renderSquare(8)}
+        {this.renderSquare(9)}
+        </div>
+      </div>
+      )
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
